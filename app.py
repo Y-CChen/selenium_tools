bda94499a30a00135d358df8135ee072d5699103
Yi-Cheng Chen <yichern.chen@gmail.com>
2020-12-20 21:37:14 +0800

    add scheduler

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import argparse
+import atexit
 import logging
+import time
 
+from apscheduler.schedulers.background import BackgroundScheduler
 from rich.logging import RichHandler
 
 from config import Config
@@ -13,11 +16,26 @@ def _main():
         logger = logging.getLogger()
         logger.setLevel(logging.getLevelName(parsed_args.logging_level.upper()))
         logger.addHandler(RichHandler(rich_tracebacks=True))
-        with make_web_driver(Config.WEB_DRIVER, Config.WEB_DRIVER_ARGS) as web_driver:
-            if hasattr(Config, "COCST_ARRIVAL_NOTICING_URLS"):
-                costco.arrival_noticing(Config.LINE_NOTIFY_ACCESS_TOKEN, web_driver, Config.COCST_ARRIVAL_NOTICING_URLS)
+
+        # scheduler
+        scheduler = BackgroundScheduler()
+        if hasattr(Config, "COCST_ARRIVAL_NOTICING_URLS"):
+            _job_costco_arrival_noticing(True)
+            scheduler.add_job(func=lambda: _job_costco_arrival_noticing(False), trigger="cron", minute="*/10")
+        scheduler.start()
+        atexit.register(lambda: scheduler.shutdown())
+
     except Exception as e:
         logging.exception(e)
+        return
+
+    while True:
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            break
+        except Exception as e:
+            logging.exception(e)
 
 
 def _parse_args(args=None):
@@ -25,6 +43,13 @@ def _parse_args(args=None):
     parser.add_argument("-ll", "--logging-level", type=str, default="info", help="logging level")
     parsed_args = parser.parse_args(args)
     return parsed_args
+
+
+def _job_costco_arrival_noticing(force_notify):
+    with make_web_driver(Config.WEB_DRIVER, Config.WEB_DRIVER_ARGS) as web_driver:
+        costco.arrival_noticing(
+            Config.LINE_NOTIFY_ACCESS_TOKEN, web_driver, Config.COCST_ARRIVAL_NOTICING_URLS, force_notify
+        )
 
 
 if __name__ == "__main__":
--- selenium_tools/costco.py
+++ selenium_tools/costco.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from .line_notify import line_notify
 
 
-def arrival_noticing(line_notify_access_token, web_driver, urls):
+def arrival_noticing(line_notify_access_token, web_driver, urls, force_notify):
     products = dict()
     web_driver_wait = WebDriverWait(web_driver, 60)
     for url in urls:
@@ -21,7 +21,7 @@ def arrival_noticing(line_notify_access_token, web_driver, urls):
         )
         in_stock = (not bool(buy_now_button.get_attribute("disabled"))) if buy_now_button else False
         products[product_name] = in_stock
-    need_notify = False
+    need_notify = force_notify
     messages = list()
     for product_name, in_stock in products.items():
         need_notify = need_notify or in_stock
